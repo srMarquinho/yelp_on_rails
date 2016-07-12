60c79ee821437b3fa7da30bcf1b2d0b9589ea9dc
srMarquinho <sr.marquinho@gmail.com>
2016-07-12 16:04:36 +0100

    12-add review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -35,6 +35,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 end
